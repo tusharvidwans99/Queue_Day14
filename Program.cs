b67a555c0332f51c346d25ac8250a3edd08ea872
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-06 04:50:01 +0530

    [Tushar Vidwans] Use LinkedList to do the Queue Operations

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Queue_Day14
+{
+    /// <summary>
+    ///Use LinkedList to do the Queue Operations. 
+    /// </summary>
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.enqueue(56);
+            queue.enqueue(30);
+            queue.enqueue(70);
+
+            queue.dequeue();
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Day14
+{
+    public class Queue<T>
+    {
+
+        LinkedList<T> queue;
+
+        //Queue constructor is used to declare linked list
+        public Queue()
+        {
+            this.queue = new LinkedList<T> ();
+        }
+
+
+
+        //enqueue method appends the data in form of linked list, @param data is a T type data which is added to list
+        public void enqueue(T data)
+        {
+            queue.AddLast(data);
+        }
+
+        //dequeue method has ability to remove the last data, used popLast method from linked list for dequeue
+        public void dequeue()
+        {
+            queue.RemoveFirst();
+        }
+    }
+}
